1c99a64d6e5420e141c9fc229aaf62ef1dee2dd5
Kaisei <me@kaisei.dev>
2022-07-25 15:20:57 +0900

    function

--- lifegame.zig
+++ lifegame.zig
@@ -10,14 +10,24 @@ fn getRandomBool() bool {
 
 fn randomCell() void {}
 
+fn showMatrix(matrix: []const bool, N: u32) void {
+    for (matrix) |_, i| {
+        print("🟩", .{});
+        if ((i + 1) % N == 0) {
+            print("\n", .{});
+        }
+    }
+}
+
 pub fn main() void {
     const N = 10;
     const M = 10;
-    const mat = [_]bool{false} ** (N * M);
+    var mat = [_]bool{false} ** (N * M);
 
     const x = getRandomBool();
     print("Hello, world!\n", .{});
     print("Hello, world!{}\n", .{mat[0]});
     print("Hello, random!{}\n", .{x});
     print("Hello, random!{}\n", .{time.nanoTimestamp()});
+    showMatrix(&mat, N);
 }
