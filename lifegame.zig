eaf8029cddf72404eb2778811da050a6c12432d4
Kaisei <me@kaisei.dev>
2022-07-25 15:05:05 +0900

    try random

--- lifegame.zig
+++ lifegame.zig
@@ -1,9 +1,23 @@
-const print = @import("std").debug.print;
+const std = @import("std");
+const print = std.debug.print;
+const DefaultPrng = std.rand.DefaultPrng;
+const time = std.time;
+
+fn getRandomBool() bool {
+    var rand = DefaultPrng.init(@intCast(u64, time.milliTimestamp()));
+    return rand.random().boolean();
+}
+
+fn randomCell() void {}
 
 pub fn main() void {
     const N = 10;
     const M = 10;
-    const row = [_]bool{false} ** N * M;
+    const mat = [_]bool{false} ** (N * M);
+
+    const x = getRandomBool();
     print("Hello, world!\n", .{});
-    print("Hello, world!{}\n", .{row[0]});
+    print("Hello, world!{}\n", .{mat[0]});
+    print("Hello, random!{}\n", .{x});
+    print("Hello, random!{}\n", .{time.nanoTimestamp()});
 }
