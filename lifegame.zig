ab73010466a45c961e61fdb6974d72fac79180df
Kaisei <me@kaisei.dev>
2022-07-25 11:55:48 +0900

    try array

--- lifegame.zig
+++ lifegame.zig
@@ -1,5 +1,9 @@
 const print = @import("std").debug.print;
 
 pub fn main() void {
+    const row = [_]bool{false} ** 10;
+    const mat = [4][4]bool{ [_]bool{false} ** 4, [_]bool{false} ** 4, [_]bool{false} ** 4, [_]bool{false} ** 4 };
     print("Hello, world!\n", .{});
+    print("Hello, world!{}\n", .{row[0]});
+    print("Hello, world!{}\n", .{mat[1][1]});
 }
