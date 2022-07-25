c41d429a82699e07fb4dbe3b1efcabf37f30403a
Kaisei <me@kaisei.dev>
2022-07-25 15:36:59 +0900

    random cells

--- lifegame.zig
+++ lifegame.zig
@@ -3,31 +3,42 @@ const print = std.debug.print;
 const DefaultPrng = std.rand.DefaultPrng;
 const time = std.time;
 
-fn getRandomBool() bool {
-    var rand = DefaultPrng.init(@intCast(u64, time.milliTimestamp()));
-    return rand.random().boolean();
-}
+const N = 10;
+const M = 10;
 
 fn randomCell() void {}
 
-fn showMatrix(matrix: []const bool, N: u32) void {
-    for (matrix) |_, i| {
-        print("🟩", .{});
+fn showMatrix(matrix: []const bool) void {
+    for (matrix) |c, i| {
+        if (c) {
+            print("🟩", .{});
+        } else {
+            print("  ", .{});
+        }
         if ((i + 1) % N == 0) {
             print("\n", .{});
         }
     }
 }
 
-pub fn main() void {
-    const N = 10;
-    const M = 10;
-    var mat = [_]bool{false} ** (N * M);
+fn getIndex(row: u32, col: u32) usize {
+    return (row * N + col);
+}
 
-    const x = getRandomBool();
+fn createUniverse(matrix: []bool) void {
+    var rand = DefaultPrng.init(@intCast(u64, time.milliTimestamp()));
+    for (matrix) |_, i| {
+        matrix[i] = rand.random().boolean();
+        print("{}\n", .{matrix[i]});
+    }
+}
+
+pub fn main() void {
+    var mat = [_]bool{false} ** (N * M);
+    createUniverse(&mat);
+
     print("Hello, world!\n", .{});
     print("Hello, world!{}\n", .{mat[0]});
-    print("Hello, random!{}\n", .{x});
     print("Hello, random!{}\n", .{time.nanoTimestamp()});
-    showMatrix(&mat, N);
+    showMatrix(&mat);
 }
